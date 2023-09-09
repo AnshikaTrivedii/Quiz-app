aca9d14dacfed3035c1fa1e485117503426a402a
AnshikaTrivedii <trivedianshika48@gmail.com>
2023-09-09 14:58:49 +0530

    quiz

---
+++ Quiz App/src/Helpers/QuestionBank.jsx
@@ -0,0 +1,29 @@
+export const Questions = [
+  {
+    prompt: "What is 1+3?",
+    optionA: "21",
+    optionB: "14",
+    optionC: "2",
+    optionD: "4",
+    answer: "D",
+  },
+
+  {
+    prompt: "What is a Streaming Platform?",
+    optionA: "Twitter",
+    optionB: "Twitch",
+    optionC: "Google",
+    optionD: "Robinhoood",
+    answer: "B",
+  },
+
+  {
+    prompt: "What is 1+5?",
+    optionA: "6",
+    optionB: "34",
+    optionC: "14",
+    optionD: "10",
+    answer: "A",
+  },
+
+];
